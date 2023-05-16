bff1fe6a380ab6d1c1b43e2d0c16a5696a41d8d7
Carlos Velasquez <cvelasquezr8@gmail.com>
2023-05-16 07:34:42 -0400

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cvelasquezr8.github.io",
-  base: "/equasystems-website-project",
-  output: "server",
-  adapter: netlify()
+  // site: "https://cvelasquezr8.github.io",
+  // base: "/equasystems-website-project",
+  // output: "server",
+  // adapter: netlify()
 });

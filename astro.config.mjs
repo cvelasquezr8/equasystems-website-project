4c0fa680c91dac83f48d169908653ca319341a3d
Carlos Velasquez <cvelasquezr8@gmail.com>
2023-05-16 07:04:05 -0400

    Adding config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://cvelasquezr8.github.io",
   base: "/equasystems-website-project",
+  output: "server",
+  adapter: netlify()
 });
